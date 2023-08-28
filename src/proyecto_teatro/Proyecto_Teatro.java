b8c366947816bb65b5b9484b65ac1b3aa4955e68
SantiagoVargas <santiago19267@gmail.com>
2023-08-27 21:56:58 -0500

    Proyecto de Teatro en JAVA

---
+++ src/proyecto_teatro/Proyecto_Teatro.java
@@ -0,0 +1,22 @@
+package proyecto_teatro;
+
+/**
+ *
+ * @author SENA
+ */
+import java.util.Scanner;
+public class Proyecto_Teatro {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int edad,cantidad,total;
+        total=500000;
+        System.out.println("ingrese la cantidad de personas");
+        cantidad=sc.nextInt();
+        do{
+        System.out.println("ingrese la edad");
+        edad=sc.nextInt();
+        }while(cantidad=);
+            
+    }
+    
+}
